2172fc28c080fbcb8c4d286c72859f67088b8c1a
nate zhou <gnuunixchad@outlook.com>
2026-02-10 11:43:02 +0800

    Fix(config.def.h): bar nerd fonts size

--- config.def.h
+++ config.def.h
@@ -6,7 +6,10 @@ static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceCodePro:medium:size=13" };
+static const char *fonts[]          = {
+    "SourceCodePro:medium:size=13",
+    "SymbolsNerdFont:normal:size=13"
+};
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#bbbbbb";
